f328bf9cf07a484d3ccfdad1dcdea4371b42dce0
Dev Karan <81677957+Kev-Daran@users.noreply.github.com>
2022-01-29 18:52:24 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
 'use strict';
 
-console.log(document.querySelector('.message'));
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.score').textContent = score;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  //When there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔No number';
+  }
+  //When player wins
+  else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+  }
+  //When guess is too high
+  else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈Too high!';
+    score--;
+    document.querySelector('.score').textContent = score;
+    if (score < 1) {
+      document.querySelector('.message').textContent = '💥You lost!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+  //When guess is too low
+  else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉Too low!';
+    score--;
+    document.querySelector('.score').textContent = score;
+    if (score < 1) {
+      document.querySelector('.message').textContent = '💥You lost!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
